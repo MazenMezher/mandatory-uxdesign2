9bfbcf84999682c98c320a8dbe849ea0c7b3c670
Liwaa Mazen Mezher <usbminnet@hotmail.com>
2020-03-26 16:09:24 +0100

    backup

--- src/Components/Hamburger.js
+++ src/Components/Hamburger.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class Hamburger extends Component {
     render() {
         return (
-             <>
+            <>
                 <div id="nav">
                     <input type="checkbox" />
                     <span></span>
